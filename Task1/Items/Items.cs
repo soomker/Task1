a3a507e47ac3a236a89410375bc5d92c671bb7d4
soomker <soomker@gmail.com>
2016-07-08 00:17:26 +0300

    fix

---
+++ Task1/Items/Items.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Items
+{
+  public  class Item
+  {
+      #region -------------- CLASS FIELDS -------------------
+      
+      string itemName;
+      int itemPosX;
+      int itemPosY;
+      int totalPos;
+      
+      #endregion -------------- CLASS FIELDS -------------------
+
+       #region -------------- CLASS PROPERTIES -------------------
+ 
+      /// <summary>
+     /// Sum of Positions (need for check reserved positions)
+    /// </summary>
+      public int TotalPos
+      {
+          get { return totalPos; }
+      }
+     
+       #endregion -------------- CLASS PROPERTIES -------------------
+
+      #region -------------- CLASS CONSTRUCTOR -------------------
+
+      public Item(string name, int posX, int posY)
+      {
+          itemName = name;
+          itemPosX = posX;
+          itemPosY = posY;
+          totalPos = posX + posY;
+      }
+
+      #endregion -------------- CLASS CONSTRUCTOR -------------------
+
+      #region -------------- CLASS METHODS -------------------
+
+      public virtual void SaySomething(){}
+    
+     
+      public void Move(int [] positions)
+        {
+            Console.Write(String.Format(itemName + " Current position is: X - {0} and Y - {1} ", itemPosX.ToString(), itemPosY.ToString()));
+            Random rand = new Random();
+            do
+            {
+                itemPosX = rand.Next(0, 100);
+                itemPosY = rand.Next(0, 100);
+                totalPos = itemPosX + itemPosY;
+            }
+            while (positions.Contains(totalPos));
+            Console.Write(String.Format(itemName + " Position after move is: X - {0} and Y - {1}", itemPosX.ToString(), itemPosY.ToString()));
+            Console.WriteLine();
+            totalPos = itemPosX + itemPosY;
+        }
+
+      #endregion -------------- CLASS METHODS -------------------
+  }
+
+ public class Animal : Item
+    {
+     string animalSpeech;  
+     public Animal(string name, int posX, int posY, string speech)
+            : base(name,posX,posY)
+        {
+            animalSpeech = speech;
+        }
+
+        public override void SaySomething()
+        {
+            Console.Write(animalSpeech);
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1.Items;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item chair = new Item("chair", 20, 30);
+            Item table = new Item("table", 40, 50);
+            Item tv = new Item("tv", 25, 55);
+            Item pc = new Item("pc", 44, 0);
+            Item couch = new Item("couch", 10, 16);
+            Animal cat = new Animal("cat", 10, 16, "Meow!!");
+
+            List<Item> roomItems = new List<Item> { chair, table, tv, pc, couch, cat };
+            int[] reservPos = new int[roomItems.Count];
+
+            for (int i = 0; i < roomItems.Count; i++)
+            {
+                reservPos[i] = roomItems[i].TotalPos;
+            }
+
+            ConsoleKeyInfo key;
+            
+            doMove:
+            Console.WriteLine();
+            Console.WriteLine("To move Items press 'ENTER' or double press 'Esc' to exit");
+            if ((key = Console.ReadKey(true)).Key ==ConsoleKey.Enter)
+            {
+
+                MoveItems(roomItems, ref reservPos);
+                goto doMove;
+            }
+            if ((key = Console.ReadKey(true)).Key == ConsoleKey.Escape) return;
+                
+            
+           
+        }
+        static void MoveItems(List<Item> roomItems, ref int [] reservedPos)
+        {
+                for (int i = 0; i < roomItems.Count; i++)
+                {
+                    roomItems[i].SaySomething();
+                    roomItems[i].Move(reservedPos);
+                    reservedPos[i] = roomItems[i].TotalPos;
+                }
+               
+        }
+    }
+}
